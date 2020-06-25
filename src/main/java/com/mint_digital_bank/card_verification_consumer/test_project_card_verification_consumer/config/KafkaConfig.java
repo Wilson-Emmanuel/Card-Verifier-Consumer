20a7e91a980910cb0b60faca1b444e1ce011b43c
Wilson-Emmanuel <wilsonemmanuel144@gmail.com>
2020-06-25 12:11:06 +0100

    Final refactoring

--- src/main/java/com/mint_digital_bank/card_verification_consumer/test_project_card_verification_consumer/config/KafkaConfig.java
+++ src/main/java/com/mint_digital_bank/card_verification_consumer/test_project_card_verification_consumer/config/KafkaConfig.java
@@ -1,17 +1,14 @@
 package com.mint_digital_bank.card_verification_consumer.test_project_card_verification_consumer.config;
 
-import com.mint_digital_bank.card_verification_consumer.test_project_card_verification_consumer.models.Card;
 import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.common.serialization.StringDeserializer;
-import org.apache.kafka.common.serialization.StringSerializer;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.annotation.EnableKafka;
 import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
 import org.springframework.kafka.core.ConsumerFactory;
 import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
-import org.springframework.kafka.support.serializer.JsonDeserializer;
-import org.springframework.kafka.support.serializer.JsonSerializer;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -20,19 +17,21 @@ import java.util.Map;
 @EnableKafka
 public class KafkaConfig {
 
+    @Value(value = "${kafka.bootstrapAddress}")
+    private String bootstrapAddress;
     @Bean
-    public ConsumerFactory<String, Card> consumerFactory(){
+    public ConsumerFactory<String, String> consumerFactory(){
         Map<String,Object> config = new HashMap<>();
-        config.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG,"127.0.0.1:9092");
+        config.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG,bootstrapAddress);
         config.put(ConsumerConfig.GROUP_ID_CONFIG, "mint_group_id");
-        config.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringSerializer.class);
-        config.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, JsonSerializer.class);
-        return new DefaultKafkaConsumerFactory<>(config,new StringDeserializer(), new JsonDeserializer<>(Card.class));
+        config.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+        config.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+        return new DefaultKafkaConsumerFactory<>(config );
     }
 
     @Bean
-    public ConcurrentKafkaListenerContainerFactory<String,Card> cardConcurrentKafkaListenerContainerFactory(){
-        ConcurrentKafkaListenerContainerFactory<String,Card> factory = new ConcurrentKafkaListenerContainerFactory<>();
+    public ConcurrentKafkaListenerContainerFactory<String,String> cardConcurrentKafkaListenerContainerFactory(){
+        ConcurrentKafkaListenerContainerFactory<String,String> factory = new ConcurrentKafkaListenerContainerFactory<>();
         factory.setConsumerFactory(consumerFactory());
         return factory;
     }
--- src/main/java/com/mint_digital_bank/card_verification_consumer/test_project_card_verification_consumer/services/KafkaConsumer.java
+++ src/main/java/com/mint_digital_bank/card_verification_consumer/test_project_card_verification_consumer/services/KafkaConsumer.java
@@ -1,14 +1,13 @@
 package com.mint_digital_bank.card_verification_consumer.test_project_card_verification_consumer.services;
 
-import com.mint_digital_bank.card_verification_consumer.test_project_card_verification_consumer.models.Card;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
 
 @Service
 public class KafkaConsumer {
 
-    @KafkaListener(topics = "com.ng.vela.even.card_verified",groupId="mint_group_id")
-  public void consume(Card card){
+    @KafkaListener(topics = "com.ng.vela.even.card_verified",groupId="mint_group_id", containerFactory = "cardConcurrentKafkaListenerContainerFactory")
+  public void consume(String card){
       System.out.println(card);
   }
 }
